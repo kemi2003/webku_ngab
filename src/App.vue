a3d81c4ea400692e48a3f3b0f8341e22b4b73a71
kemi2003 <ahmedaprianto@students.amikom.ac.id>
2023-06-11 21:48:28 +0700

    tes lagi bolo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <TugasBootstrap />
+  <!-- <HomeBootstrap /> -->
+  <HomeAku />
+  <h1>Nama saya adalah {{ nama }}</h1>
+  <h1>Umur saya adalah {{ umur }}</h1>
+  <h1>{{ bilangan }}</h1>
+  <button v-on:click="kali">Kali</button>
+  <button v-on:click="kurang">Kurang</button>
+  <button v-on:click="tambah">Tambah</button>
+  <button v-on:click="bagi">Bagi</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeAku from "./components/HomeAku.vue";
+import TugasBootstrap from "./components/TugasBootstrap.vue";
+// import HomeBootstrap from "./components/HomeBootstrap.vue";
+
+// import HelloWorld from './components/HelloWorld.vue'
+// import HomeDua from './components/HomeDua.vue'
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      nama: "aldi Taher",
+      umur: 20,
+      bilangan: 1,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    HomeAku,
+    TugasBootstrap,
+  },
+  methods: {
+    tambah() {
+      this.bilangan += 1;
+    },
+    kurang() {
+      this.bilangan -= 1;
+    },
+    kali() {
+      this.bilangan *= 10;
+    },
+    bagi() {
+      this.bilangan /= 10;
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,6 +57,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
